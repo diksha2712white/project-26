0161c6366f62f6783cc865443dddf865d376cc0e
diksha rani <mailmediksha.rani@gmail.com>
2020-10-19 15:18:38 +0530

    reciepe added

--- sketch.js
+++ sketch.js
@@ -34,7 +34,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+bird2=new Bird(200,400);
 }
 
 function draw(){
@@ -57,6 +57,6 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+bird2.display();
     bird.display();
 }
